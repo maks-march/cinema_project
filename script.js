fdb85a7f4bb419abcc18c38cb3949faf12cb0a80
maks-march <1maks_2055@mail.ru>
2022-02-06 14:06:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function showMovies(movies) {
         <div class="overview">
         <h3>Обзор</h3>
         ${overview}
+        </br><a class= "hyperlink" href = "https://www.kinopoisk.ru/index.php?kp_query=${title}">Ссылка на кинопоиск.</a>
         </div>
         `
         main.appendChild(movieEl)
