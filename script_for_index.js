29b22beeb139bf87f77db6a46eb7b313c5a4c58d
maks-march <1maks_2055@mail.ru>
2022-03-10 20:40:39 +0500

    1/0

---
+++ script_for_index.js
@@ -0,0 +1,6 @@
+START_btn = document.querySelector('.START');
+
+START_btn.addEventListener('click', ()=>{
+    window.location.href = 'main.html';
+});
+
---
+++ script_for_main.js
@@ -0,0 +1,77 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+const rec = document.querySelector('.rec');
+const home = document.querySelector('.home');
+
+
+
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=42e6fa3f7868233b9d9dc6a7f1e3c93a&page=1'
+const API_URL_rec = 'https://api.themoviedb.org/3/discover/movie?sort_by=vote_count.desc&api_key=42e6fa3f7868233b9d9dc6a7f1e3c93a&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=42e6fa3f7868233b9d9dc6a7f1e3c93a&query='
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+    const result = await fetch(url);
+    const data = await result.json();
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+    movies.forEach((movie) => {
+        
+        const {title, poster_path, vote_average, overview, adult} = movie;
+        if (poster_path !== null && overview !== '') {
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+        <h3>Review</h3>
+        ${overview}
+        </br>
+        <a class= "hyperlink" href = "https://www.kinopoisk.ru/index.php?kp_query=${title}">Watch on kinopoisk.</a>
+        </div>
+        `
+        main.appendChild(movieEl);    
+        }
+    }
+    );
+}
+
+function getClassByRate(vote_average) {
+    if(vote_average>=8) {
+        return 'green';
+    } else if (vote_average>=5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = search.value;
+    if(searchTerm && search.term !== '') {
+        main.innerHTML = '';
+        getMovies(SEARCH_API + searchTerm);
+    } else {
+        window.location.reload();
+    }
+});
+
+home.addEventListener('click',()=>{
+    main.innerHTML = '';
+    getMovies(API_URL);
+});
+rec.addEventListener('click',()=>{
+    main.innerHTML = '';
+    getMovies(API_URL_rec);
+});
