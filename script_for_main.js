86d26ce0117b40a03b4c91c8cf6433934d817e4a
maks-march <1maks_2055@mail.ru>
2022-04-17 21:01:57 +0500

    1.0

--- script_for_main.js
+++ script_for_main.js
@@ -6,38 +6,75 @@ const home = document.querySelector('.home');
 
 
 
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=42e6fa3f7868233b9d9dc6a7f1e3c93a&page=1'
-const API_URL_rec = 'https://api.themoviedb.org/3/discover/movie?sort_by=vote_count.desc&api_key=42e6fa3f7868233b9d9dc6a7f1e3c93a&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=42e6fa3f7868233b9d9dc6a7f1e3c93a&query='
+const API_URL_top = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top'
+const API_URL_new = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres'
+const API_URL_search = "https://kinopoiskapiunofficial.tech/api/v2.2/films/search-by-keyword"
 
-getMovies(API_URL);
+getMovies(API_URL_top);
 
-async function getMovies(url) {
-    const result = await fetch(url);
-    const data = await result.json();
-    showMovies(data.results);
+async function getMovies(URL,searchTerm) {
+    const data = [];
+    if (searchTerm == ''){
+        const result = await fetch(URL, {
+           method: 'GET',
+           headers: {
+                'X-API-KEY': '79012899-2016-4077-9388-1ce05b3d8677',
+                'Content-Type': 'application/json',
+               },
+        })
+        data.push(await result.json());
+    } else {
+        const result = await fetch(URL, {
+            method: 'GET',
+            headers: {
+                'X-API-KEY': '79012899-2016-4077-9388-1ce05b3d8677',
+                'Content-Type': 'application/json',
+                'keyword': searchTerm,
+            },
+        })
+        data.push(await result.json());
+    }
+    film = data[0]['films'];
+    
+    film.forEach((movie) => {
+        const {filmId} = movie;
+        const result = fetch('https://kinopoiskapiunofficial.tech/api/v1/reviews', {
+            method: 'GET',
+            headers: {
+                'X-API-KEY': '79012899-2016-4077-9388-1ce05b3d8677',
+                'Content-Type': 'application/json',
+                'filmId': filmId,
+            },
+        });
+        //movie.push(await result.json());
+
+    })
+    console.log(data[0]['films']) 
+    showMovies(data);
 }
+
+
+
 
 function showMovies(movies) {
     main.innerHTML = '';
-    movies.forEach((movie) => {
-        
-        const {title, poster_path, vote_average, overview, adult} = movie;
-        if (poster_path !== null && overview !== '') {
+    film = movies[0]['films'];
+    film.forEach((movie) => {
+        const {nameRu, posterUrl, rating, description} = movie;
+        if (posterUrl !== null && description !== '') {
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
-        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <img src="${posterUrl}" alt="">
         <div class="movie-info">
-        <h3>${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        <h3>${nameRu}</h3>
+        <span class="${getClassByRate(rating)}">${rating}</span>
         </div>
         <div class="overview">
-        <h3>Review</h3>
-        ${overview}
+        <h3>Описание</h3>
+        ${description}
         </br>
-        <a class= "hyperlink" href = "https://www.kinopoisk.ru/index.php?kp_query=${title}">Watch on kinopoisk.</a>
+        <a class= "hyperlink" href = "https://www.kinopoisk.ru/index.php?kp_query=${nameRu}">Смотреть на кинопоиске.</a>
         </div>
         `
         main.appendChild(movieEl);    
@@ -59,9 +96,9 @@ function getClassByRate(vote_average) {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const searchTerm = search.value;
-    if(searchTerm && search.term !== '') {
+    if(searchTerm !== '') {
         main.innerHTML = '';
-        getMovies(SEARCH_API + searchTerm);
+        getMovies(API_URL_search,searchTerm);
     } else {
         window.location.reload();
     }
@@ -69,9 +106,9 @@ form.addEventListener('submit', (e) => {
 
 home.addEventListener('click',()=>{
     main.innerHTML = '';
-    getMovies(API_URL);
+    getMovies(API_URL_top);
 });
 rec.addEventListener('click',()=>{
     main.innerHTML = '';
-    getMovies(API_URL_rec);
+    getMovies(API_URL_new);
 });
